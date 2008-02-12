5be1b4db652ea13cf84571b05cedf4c6c7310d6f
Kilian Thiel <kilian.thiel@knime.com>
2008-02-12 09:33:46 +0000

    Initial import.

---
+++ org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/Activator.java
@@ -0,0 +1,52 @@
+package org.knime.ext.textprocessing;
+
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.BundleContext;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class Activator extends AbstractUIPlugin {
+
+    /**
+     * The plug-in ID.
+     */
+	public static final String PLUGIN_ID = "org.knime.ext.textprocessing";
+
+	// The shared instance
+	private static Activator plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public Activator() {
+	    plugin = this;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void start(final BundleContext context) throws Exception {
+		super.start(context);
+		plugin = this;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void stop(final BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static Activator getDefault() {
+		return plugin;
+	}
+}
