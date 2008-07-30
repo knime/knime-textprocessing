341b401001778ba6cfc985257bbfc03637413f18
Pierre-Francois Laquerre <unknown@uni.kn>
2008-07-30 10:09:44 +0000

    Removed redundant contains() calls.

--- org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/nodes/transformation/bow/BagOfWordsNodeModel.java
+++ org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/nodes/transformation/bow/BagOfWordsNodeModel.java
@@ -17,7 +17,7 @@
  * website: www.knime.org
  * email: contact@knime.org
  * ---------------------------------------------------------------------
- * 
+ *
  * History
  *   03.03.2008 (Kilian Thiel): created
  */
@@ -27,7 +27,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Hashtable;
-import java.util.List;
+import java.util.Iterator;
 import java.util.Set;
 
 import org.knime.core.data.DataCell;
@@ -44,8 +44,6 @@ import org.knime.core.node.NodeSettingsRO;
 import org.knime.core.node.NodeSettingsWO;
 import org.knime.ext.textprocessing.data.Document;
 import org.knime.ext.textprocessing.data.DocumentValue;
-import org.knime.ext.textprocessing.data.Paragraph;
-import org.knime.ext.textprocessing.data.Section;
 import org.knime.ext.textprocessing.data.Sentence;
 import org.knime.ext.textprocessing.data.Term;
 import org.knime.ext.textprocessing.util.BagOfWordsDataTableBuilder;
@@ -53,24 +51,24 @@ import org.knime.ext.textprocessing.util.DataTableBuilderFactory;
 import org.knime.ext.textprocessing.util.DataTableSpecVerifier;
 
 /**
- * The model class of the Bag of word creator node. The method 
+ * The model class of the Bag of word creator node. The method
  * {@link BagOfWordsNodeModel#configure(DataTableSpec[])} validates the incoming
  * {@link org.knime.core.data.DataTableSpec}. Only one column containing
  * {@link org.knime.ext.textprocessing.data.DocumentCell}s is allowed.
- * The output table contains two columns. The first consists of 
+ * The output table contains two columns. The first consists of
  * {@link org.knime.ext.textprocessing.data.TermCell}s the second of
- * {@link org.knime.ext.textprocessing.data.DocumentCell}s. A single row 
+ * {@link org.knime.ext.textprocessing.data.DocumentCell}s. A single row
  * represents a tupel of a term and a document meaning that the term is
  * contained in the document.
- * 
+ *
  * @author Kilian Thiel, University of Konstanz
  */
 public class BagOfWordsNodeModel extends NodeModel {
 
     private int m_documentColIndex = -1;
-    
+
     private BagOfWordsDataTableBuilder m_dtBuilder;
-    
+
     /**
      * Creates a new instance of <code>BagOfWordsNodeModel</code> with one in
      * and one data table out port.
@@ -79,107 +77,90 @@ public class BagOfWordsNodeModel extends NodeModel {
         super(1, 1);
         m_dtBuilder = DataTableBuilderFactory.createBowDataTableBuilder();
     }
-    
+
     /**
      * {@inheritDoc}
      */
     @Override
-    protected DataTableSpec[] configure(DataTableSpec[] inSpecs)
+    protected DataTableSpec[] configure(final DataTableSpec[] inSpecs)
             throws InvalidSettingsException {
         checkDataTableSpec(inSpecs[0]);
         return new DataTableSpec[]{m_dtBuilder.createDataTableSpec()};
     }
 
-    private void checkDataTableSpec(final DataTableSpec spec) 
+    private void checkDataTableSpec(final DataTableSpec spec)
     throws InvalidSettingsException {
         DataTableSpecVerifier verifier = new DataTableSpecVerifier(spec);
         verifier.verifyDocumentCell(true);
-        m_documentColIndex = verifier.getDocumentCellIndex();        
+        m_documentColIndex = verifier.getDocumentCellIndex();
     }
-    
+
     /**
      * {@inheritDoc}
      */
     @Override
     protected BufferedDataTable[] execute(final BufferedDataTable[] inData,
-            ExecutionContext exec) throws Exception {
+            final ExecutionContext exec) throws Exception {
         checkDataTableSpec(inData[0].getDataTableSpec());
-        
-       Hashtable<Document, Set<Term>> docTerms = 
+
+       Hashtable<Document, Set<Term>> docTerms =
            new Hashtable<Document, Set<Term>>();
-       Hashtable<DataCell, Set<Term>> docCellTerms = 
+       Hashtable<DataCell, Set<Term>> docCellTerms =
            new Hashtable<DataCell, Set<Term>>();
-       
+
        ExecutionMonitor subExec = exec.createSubProgress(0.5);
        int rowCount = inData[0].getRowCount();
        int currRow = 1;
-       RowIterator it = inData[0].iterator();       
+       RowIterator it = inData[0].iterator();
        while (it.hasNext()) {
            DataRow row = it.next();
-           
+
            // get terms for document
-           DocumentValue docCell = 
-               (DocumentValue)row.getCell(m_documentColIndex); 
+           DocumentValue docCell =
+               (DocumentValue)row.getCell(m_documentColIndex);
            Document doc = docCell.getDocument();
            Set<Term> terms = setOfTerms(doc);
-           
+
            // add data cell and corresponding terms
            docTerms.put(doc, terms);
            docCellTerms.put(row.getCell(m_documentColIndex), terms);
-           
+
            // report status
            double progress = (double)currRow / (double)rowCount;
-           subExec.setProgress(progress, "Processing document " + currRow + " of " 
+           subExec.setProgress(progress, "Processing document " + currRow + " of "
                    + rowCount);
            exec.checkCanceled();
            currRow++;
        }
-       
+
        // build data table
        ExecutionContext subContext = exec.createSubExecutionContext(0.5);
-       
+
        // Do not reuse the DocumentCells of the previous DataTable
 //       return new BufferedDataTable[]{m_dtBuilder.createDataTable(
 //               subContext, docTerms, false)};
-       
+
        // Do reuse the DocumentCells of the previous DataTable
        return new BufferedDataTable[]{m_dtBuilder.createReusedDataTable(
                subContext, docCellTerms, true)};
     }
-    
-    
+
+
     private Set<Term> setOfTerms(final Document doc) {
         Set<Term> termSet = null;
         if (doc != null) {
             termSet = new HashSet<Term>();
-            
-            // All Sections
-            List<Section> secs = doc.getSections();
-            for (Section s : secs) {
-                
-                // All Paragraphs
-                List<Paragraph> paras = s.getParagraphs();
-                for (Paragraph p : paras) {
-                    
-                    // All Sentences
-                    List<Sentence> sentences = p.getSentences();
-                    for (Sentence sen : sentences) {
-                        
-                        // All Terms
-                        List<Term> terms = sen.getTerms();
-                        for (Term t : terms) {
-                            if (!termSet.contains(t)) {
-                                termSet.add(t);
-                            }
-                        }
-                    }
-                }
+
+            Iterator<Sentence> it = doc.sentenceIterator();
+            while (it.hasNext()) {
+                Sentence sen = it.next();
+                termSet.addAll(sen.getTerms());
             }
         }
         return termSet;
     }
-    
-    
+
+
     /**
      * {@inheritDoc}
      */
@@ -194,7 +175,7 @@ public class BagOfWordsNodeModel extends NodeModel {
     protected void validateSettings(final NodeSettingsRO settings)
             throws InvalidSettingsException {
     }
-    
+
     /**
      * {@inheritDoc}
      */
@@ -202,8 +183,8 @@ public class BagOfWordsNodeModel extends NodeModel {
     protected void loadValidatedSettingsFrom(final NodeSettingsRO settings)
             throws InvalidSettingsException {
     }
-    
-    
+
+
 
     /**
      * {@inheritDoc}
@@ -216,7 +197,7 @@ public class BagOfWordsNodeModel extends NodeModel {
      * {@inheritDoc}
      */
     @Override
-    protected void saveInternals(final File nodeInternDir, 
+    protected void saveInternals(final File nodeInternDir,
             final ExecutionMonitor exec)
             throws IOException, CanceledExecutionException {
     }
@@ -225,7 +206,7 @@ public class BagOfWordsNodeModel extends NodeModel {
      * {@inheritDoc}
      */
     @Override
-    protected void loadInternals(final File nodeInternDir, 
+    protected void loadInternals(final File nodeInternDir,
             final ExecutionMonitor exec)
             throws IOException, CanceledExecutionException {
     }
