737f76aea98e1e143d971e3065dbf555e2d877d1
Kilian Thiel <kilian.thiel@knime.com>
2008-08-08 12:41:24 +0000

    Added node "Term Vector" which creates a term vector for each term

---
+++ org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/nodes/transformation/termvector/TermVectorConfigKeys.java
@@ -0,0 +1,42 @@
+/* ------------------------------------------------------------------
+ * This source code, its documentation and all appendant files
+ * are protected by copyright law. All rights reserved.
+ *
+ * Copyright, 2003 - 2008
+ * University of Konstanz, Germany
+ * Chair for Bioinformatics and Information Mining (Prof. M. Berthold)
+ * and KNIME GmbH, Konstanz, Germany
+ *
+ * You may not modify, publish, transmit, transfer or sell, reproduce,
+ * create derivative works from, distribute, perform, display, or in
+ * any way exploit any of the content, in whole or in part, except as
+ * otherwise expressly permitted in writing by the copyright owner or
+ * as specified in the license file distributed with this product.
+ *
+ * If you have any questions please contact the copyright holder:
+ * website: www.knime.org
+ * email: contact@knime.org
+ * ---------------------------------------------------------------------
+ * 
+ * History
+ *   06.05.2008 (thiel): created
+ */
+package org.knime.ext.textprocessing.nodes.transformation.termvector;
+
+/**
+ * Provides the configuration keys for the document vector node.
+ * 
+ * @author Kilian Thiel, University of Konstanz
+ */
+public class TermVectorConfigKeys {
+
+    /**
+     * The configuration key of the boolean value setting.
+     */
+    public static final String CFGKEY_BOOLEAN = "Boolean";
+    
+    /**
+     * The configuration key of the column value setting.
+     */
+    public static final String CFGKEY_VALUE_COL = "Value_Col";
+}
