6978525ec41c4de84d57a9dae17e9b3115d596bc
Kilian Thiel <kilian.thiel@knime.com>
2009-06-24 14:38:12 +0000

    Fixed compare bug: DocumentValueComparator just compared title and text, now it compares every attribute of a document.

--- org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/data/DocumentValueComparator.java
+++ org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/data/DocumentValueComparator.java
@@ -48,6 +48,8 @@ public class DocumentValueComparator extends DataValueComparator {
         if (v1 == v2) {
             return 0;
         }
+        
+        // compare title
         String str1 = ((DocumentValue)v1).getDocument().getTitle();
         String str2 = ((DocumentValue)v2).getDocument().getTitle();
         int res = str1.compareTo(str2);
@@ -55,8 +57,62 @@ public class DocumentValueComparator extends DataValueComparator {
             return res;
         }
         
+        // compare type
+        str1 = ((DocumentValue)v1).getDocument().getType().toString();
+        str2 = ((DocumentValue)v2).getDocument().getType().toString();
+        res = str1.compareTo(str2);
+        if (res != 0) {
+            return res;
+        }
+        
+        // compare date
+        str1 = ((DocumentValue)v1).getDocument().getPubDate().toString();
+        str2 = ((DocumentValue)v2).getDocument().getPubDate().toString();
+        res = str1.compareTo(str2);
+        if (res != 0) {
+            return res;
+        }
+        
+        // compare file
+        str1 = ((DocumentValue)v1).getDocument().getDocFile().getAbsolutePath();
+        str2 = ((DocumentValue)v2).getDocument().getDocFile().getAbsolutePath();
+        res = str1.compareTo(str2);
+        if (res != 0) {
+            return res;
+        }
+        
+        // compare authors
+        str1 = ((DocumentValue)v1).getDocument().getAuthors().toString();
+        str2 = ((DocumentValue)v2).getDocument().getAuthors().toString();
+        res = str1.compareTo(str2);
+        if (res != 0) {
+            return res;
+        }
+        
+        // compare categories
+        str1 = ((DocumentValue)v1).getDocument().getCategories().toString();
+        str2 = ((DocumentValue)v2).getDocument().getCategories().toString();
+        res = str1.compareTo(str2);
+        if (res != 0) {
+            return res;
+        }
+
+        // compare sources
+        str1 = ((DocumentValue)v1).getDocument().getSources().toString();
+        str2 = ((DocumentValue)v2).getDocument().getSources().toString();
+        res = str1.compareTo(str2);
+        if (res != 0) {
+            return res;
+        }
+        
+        // finally compare text (because its the most expensive comparison
         str1 = ((DocumentValue)v1).getDocument().getText();
         str2 = ((DocumentValue)v2).getDocument().getText();
+        res = str1.compareTo(str2);
+        if (res != 0) {
+            return res;
+        }
+        
         return str1.compareTo(str2);
     }
 }
