9e69ae63c6c36adbf97627029564fe76307e418e
Kilian Thiel <kilian.thiel@knime.com>
2009-10-21 09:11:52 +0000

    Maxent model have been set to SoftReferences in order to delete the model and free memory.

--- org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/nodes/tagging/opennlpner/OpenNlpModel.java
+++ org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/nodes/tagging/opennlpner/OpenNlpModel.java
@@ -1,24 +1,31 @@
 /* -------------------------------------------------------------------
  * This source code, its documentation and all appendant files
  * are protected by copyright law. All rights reserved.
- * 
+ *
  * Copyright, 2003 - 2006
  * Universitaet Konstanz, Germany.
  * Lehrstuhl fuer Angewandte Informatik
  * Prof. Dr. Michael R. Berthold
- * 
+ *
  * You may not modify, publish, transmit, transfer or sell, reproduce,
  * create derivative works from, distribute, perform, display, or in
  * any way exploit any of the content, in whole or in part, except as
  * otherwise expressly permitted in writing by the copyright owner.
  * -------------------------------------------------------------------
- * 
+ *
  * History
  *   28.09.2009 (thiel): created
  */
 package org.knime.ext.textprocessing.nodes.tagging.opennlpner;
 
+import java.io.File;
+import java.io.IOException;
+import java.lang.ref.SoftReference;
+
 import opennlp.maxent.MaxentModel;
+import opennlp.maxent.io.SuffixSensitiveGISModelReader;
+
+import org.knime.core.node.NodeLogger;
 
 /**
  * @author Kilian Thiel, University of Konstanz
@@ -26,26 +33,32 @@ import opennlp.maxent.MaxentModel;
  */
 public class OpenNlpModel {
 
+    private static final NodeLogger LOGGER =
+        NodeLogger.getLogger(OpenNlpModel.class);
+
     private String m_name;
-    
-    private MaxentModel m_model;
-    
+
+    private SoftReference<MaxentModel> m_model;
+
+    private String m_fileName;
+
     private String m_tag;
 
     /**
-     * Creates new instance of <code>OpenNlpModel</code> with given name, model 
+     * Creates new instance of <code>OpenNlpModel</code> with given name, model
      * and tag to set.
      * @param name The model's name.
-     * @param model The maxent model.
+     * @param fileName The name of the file containing the maxent model.
      * @param tag The corresponding tag.
      */
-    public OpenNlpModel(final String name, final MaxentModel model, 
+    public OpenNlpModel(final String name, final String fileName,
             final String tag) {
         m_name = name;
-        m_model = model;
+        m_fileName = fileName;
         m_tag = tag;
+        m_model = null;
     }
-    
+
     /**
      * @return The name of the model.
      */
@@ -56,8 +69,26 @@ public class OpenNlpModel {
     /**
      * @return The maxent model.
      */
-    public MaxentModel getModel() {
-        return m_model;
+    public synchronized MaxentModel getModel() {
+        MaxentModel m = m_model == null ? null : m_model.get();
+        if (m == null) {
+            File f = new File(m_fileName);
+            if (!f.exists() || !f.isFile() || !f.canRead()) {
+                LOGGER.warn("Maxent model file [" + m_fileName
+                        + "] is not valid!");
+            }
+
+            try {
+                LOGGER.info("Loading Maxent model ["
+                        + f.getName() + "].");
+                m = new SuffixSensitiveGISModelReader(f).getModel();
+                m_model = new SoftReference<MaxentModel>(m);
+            } catch (IOException e) {
+                LOGGER.warn("Maxent model could not be loeded from file ["
+                        + m_fileName + "]!", e);
+            }
+        }
+        return m;
     }
 
     /**
@@ -65,5 +96,12 @@ public class OpenNlpModel {
      */
     public String getTag() {
         return m_tag;
+    }
+
+    /**
+     * @return The name of the file containing the maxent model.
+     */
+    public String getFileName() {
+        return m_fileName;
     }
 }
--- org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/nodes/tagging/opennlpner/OpenNlpModelFactory.java
+++ org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/nodes/tagging/opennlpner/OpenNlpModelFactory.java
@@ -1,34 +1,29 @@
 /* -------------------------------------------------------------------
  * This source code, its documentation and all appendant files
  * are protected by copyright law. All rights reserved.
- * 
+ *
  * Copyright, 2003 - 2006
  * Universitaet Konstanz, Germany.
  * Lehrstuhl fuer Angewandte Informatik
  * Prof. Dr. Michael R. Berthold
- * 
+ *
  * You may not modify, publish, transmit, transfer or sell, reproduce,
  * create derivative works from, distribute, perform, display, or in
  * any way exploit any of the content, in whole or in part, except as
  * otherwise expressly permitted in writing by the copyright owner.
  * -------------------------------------------------------------------
- * 
+ *
  * History
  *   28.09.2009 (thiel): created
  */
 package org.knime.ext.textprocessing.nodes.tagging.opennlpner;
 
-import opennlp.maxent.MaxentModel;
-import opennlp.maxent.io.SuffixSensitiveGISModelReader;
+import java.util.Hashtable;
+import java.util.Set;
 
 import org.knime.core.node.NodeLogger;
 import org.knime.ext.textprocessing.data.NamedEntityTag;
 import org.knime.ext.textprocessing.util.OpenNlpModelPaths;
-
-import java.io.File;
-import java.io.IOException;
-import java.util.Hashtable;
-import java.util.Set;
 
 /**
  * @author Kilian Thiel, University of Konstanz
@@ -38,16 +33,16 @@ public final class OpenNlpModelFactory {
 
     private static final NodeLogger LOGGER =
         NodeLogger.getLogger(OpenNlpModelFactory.class);
-    
+
     private static OpenNlpModelFactory instance = null;
-    
-    private Hashtable<String, OpenNlpModel> m_models = 
-        new Hashtable<String, OpenNlpModel>(); 
-    
+
+    private Hashtable<String, OpenNlpModel> m_models =
+        new Hashtable<String, OpenNlpModel>();
+
     /**
-     * Creates and returns a singleton instance of 
+     * Creates and returns a singleton instance of
      * <code>OpenNlpModelFactory</code>.
-     * 
+     *
      * @return a singleton instance of <code>OpenNlpModelFactory</code>.
      */
     public static final OpenNlpModelFactory getInstance() {
@@ -56,63 +51,49 @@ public final class OpenNlpModelFactory {
         }
         return instance;
     }
-    
+
     private OpenNlpModelFactory() {
-        LOGGER.info("Loading OpenNLP Models ...");
-        
+        LOGGER.info("Registering Maxent Models ...");
+
         OpenNlpModelPaths paths = OpenNlpModelPaths.getOpenNlpModelPaths();
-        
+
         // PERSON
         String name = "Person";
-        OpenNlpModel m = new OpenNlpModel(name, getModel(
-                paths.getPersonNERModelFile()), 
+        OpenNlpModel m = new OpenNlpModel(name, paths.getPersonNERModelFile(),
                 NamedEntityTag.PERSON.toString());
         m_models.put(name, m);
-        
+
         // LOCATION
         name = "Location";
-        m = new OpenNlpModel(name, getModel(paths.getLocationNERModelFile()), 
+        m = new OpenNlpModel(name, paths.getLocationNERModelFile(),
                 NamedEntityTag.LOCATION.toString());
         m_models.put(name, m);
-        
+
         // ORGANIZATION
         name = "Organization";
-        m = new OpenNlpModel(name, getModel(
-                paths.getOrganizationNERModelFile()), 
+        m = new OpenNlpModel(name, paths.getOrganizationNERModelFile(),
                 NamedEntityTag.ORGANIZATION.toString());
         m_models.put(name, m);
-        
+
         // MONEY
         name = "Money";
-        m = new OpenNlpModel(name, getModel(
-                paths.getMoneyNERModelFile()), 
+        m = new OpenNlpModel(name, paths.getMoneyNERModelFile(),
                 NamedEntityTag.MONEY.toString());
         m_models.put(name, m);
-        
+
         // DATE
         name = "Date";
-        m = new OpenNlpModel(name, getModel(
-                paths.getDateNERModelFile()), 
+        m = new OpenNlpModel(name, paths.getDateNERModelFile(),
                 NamedEntityTag.DATE.toString());
         m_models.put(name, m);
-        
+
         // TIME
         name = "Time";
-        m = new OpenNlpModel(name, getModel(
-                paths.getTimeNERModelFile()), 
+        m = new OpenNlpModel(name, paths.getTimeNERModelFile(),
                 NamedEntityTag.TIME.toString());
-        m_models.put(name, m);        
+        m_models.put(name, m);
     }
-    
-    private static MaxentModel getModel(final String name) {
-        try {
-            return new SuffixSensitiveGISModelReader(new File(name)).getModel();
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
-    
+
     /**
      * Returns the model related to the given name.
      * @param name The name to get the model for.
@@ -121,14 +102,14 @@ public final class OpenNlpModelFactory {
     public OpenNlpModel getModelByName(final String name) {
         return m_models.get(name);
     }
-    
+
     /**
      * @return A set of all names of available models.
      */
     public Set<String> getModelNames() {
         return m_models.keySet();
     }
-    
+
     /**
      * @return The name of the default model.
      */
