4b8c26ddb458384d9f66fd5ba66612e9ea6659fe
Thomas Gabriel <thomas.gabriel@knime.com>
2011-05-17 15:22:49 +0000

    textprocessing plug-in stanford-tagger clean-up

--- org.knime.ext.textprocessing.stanfordtagger/src/org/knime/ext/textprocessing/stanfordtagger/TextprocessingStanfordTaggerPlugin.java
+++ org.knime.ext.textprocessing.stanfordtagger/src/org/knime/ext/textprocessing/stanfordtagger/TextprocessingStanfordTaggerPlugin.java
@@ -1,11 +1,5 @@
 package org.knime.ext.textprocessing.stanfordtagger;
 
-import java.io.File;
-import java.net.URL;
-
-import org.eclipse.core.runtime.Path;
-import org.eclipse.core.runtime.Platform;
-import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -15,13 +9,12 @@ import org.osgi.framework.BundleContext;
 public class TextprocessingStanfordTaggerPlugin extends AbstractUIPlugin {
 
 	// The plug-in ID
-	public static final String PLUGIN_ID = "org.knime.ext.textprocessing.stanfordtagger";
+	public static final String PLUGIN_ID =
+	        "org.knime.ext.textprocessing.stanfordtagger";
 
 	// The shared instance
 	private static TextprocessingStanfordTaggerPlugin plugin;
 
-    private String m_pluginRootPath;
-	
 	/**
 	 * The constructor
 	 */
@@ -36,9 +29,6 @@ public class TextprocessingStanfordTaggerPlugin extends AbstractUIPlugin {
 	@Override
 	public void start(final BundleContext context) throws Exception {
         super.start(context);
-        final URL pluginURL = Platform.resolve(plugin.find(new Path(".")));
-        final File tmpFile = new File(pluginURL.getPath());
-        m_pluginRootPath = tmpFile.getAbsolutePath();
 	}
 
 	/**
@@ -49,32 +39,14 @@ public class TextprocessingStanfordTaggerPlugin extends AbstractUIPlugin {
         plugin = null;
         super.stop(context);
 	}
-	
+
     /**
      * Returns the shared instance.
-     * 
-     * @return The shared instance 
+     *
+     * @return The shared instance
      */
     public static TextprocessingStanfordTaggerPlugin getDefault() {
         return plugin;
-    }	
-
-    /**
-     * Returns an image descriptor for the image file at the given plug-in
-     * relative path.
-     * 
-     * @param path the path
-     * @return the image descriptor
-     */
-    public static ImageDescriptor getImageDescriptor(final String path) {
-        return AbstractUIPlugin.imageDescriptorFromPlugin(
-                "org.knime.ext.textprocessing.stanfordtagger", path);
     }
-    
-    /**
-     * @return the absolute root path of this plugin.
-     */
-    public String getPluginRootPath() {
-        return m_pluginRootPath;
-    } 	
+
 }
