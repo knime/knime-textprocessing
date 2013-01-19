ce6ab6c3d1a14812fd774c96942f93ca57fd6914
Kilian Thiel <kilian.thiel@knime.com>
2013-01-19 18:32:34 +0000

    + DictionaryReplacer holding its tokenizer instance as private member

--- org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/nodes/preprocessing/dictreplacer/DictionaryReplacer.java
+++ org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/nodes/preprocessing/dictreplacer/DictionaryReplacer.java
@@ -7,7 +7,7 @@
  *  Website: http://www.knime.org; Email: contact@knime.org
  *
  *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License, version 2, as 
+ *  it under the terms of the GNU General Public License, version 2, as
  *  published by the Free Software Foundation.
  *
  *  This program is distributed in the hope that it will be useful,
@@ -19,21 +19,22 @@
  *  with this program; if not, write to the Free Software Foundation, Inc.,
  *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  * -------------------------------------------------------------------
- * 
+ *
  * History
  *   23.09.2009 (thiel): created
  */
 package org.knime.ext.textprocessing.nodes.preprocessing.dictreplacer;
 
-import org.knime.ext.textprocessing.data.Term;
-import org.knime.ext.textprocessing.data.Word;
-import org.knime.ext.textprocessing.nodes.preprocessing.TermPreprocessing;
-import org.knime.ext.textprocessing.nodes.preprocessing.StringPreprocessing;
-import org.knime.ext.textprocessing.nodes.tokenization.DefaultTokenization;
-
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
+
+import org.knime.ext.textprocessing.data.Term;
+import org.knime.ext.textprocessing.data.Word;
+import org.knime.ext.textprocessing.nodes.preprocessing.StringPreprocessing;
+import org.knime.ext.textprocessing.nodes.preprocessing.TermPreprocessing;
+import org.knime.ext.textprocessing.nodes.tokenization.DefaultTokenization;
+import org.knime.ext.textprocessing.nodes.tokenization.Tokenizer;
 
 /**
  * @author Kilian Thiel, University of Konstanz
@@ -42,19 +43,22 @@ import java.util.List;
 public class DictionaryReplacer implements TermPreprocessing, StringPreprocessing {
 
     private Hashtable<String, String> m_replaceDict;
-    
+
+    private Tokenizer m_wordTokenizer;
+
     /**
-     * Creates new instance of <code>DictionaryReplacer</code> with give 
+     * Creates new instance of <code>DictionaryReplacer</code> with give
      * dictionary, containing key value pairs for replacement.
-     * 
+     *
      * @param replaceDict The dictionary consisting of key value pairs for
      * replacement (keys will be replaced by their corresponding values).
      */
     public DictionaryReplacer(final Hashtable<String, String> replaceDict) {
         super();
         m_replaceDict = replaceDict;
+        m_wordTokenizer = DefaultTokenization.getWordTokenizer();
     }
-    
+
     /**
      * {@inheritDoc}
      */
@@ -63,9 +67,8 @@ public class DictionaryReplacer implements TermPreprocessing, StringPreprocessin
         String word = term.getText();
         String newWord = m_replaceDict.get(word);
         if (newWord != null) {
-            List<String> tokenizedWords = 
-                DefaultTokenization.getWordTokenizer().tokenize(newWord);
-            
+            List<String> tokenizedWords = m_wordTokenizer.tokenize(newWord);
+
             List<Word> newWords = new ArrayList<Word>();
             for (String s : tokenizedWords) {
                 newWords.add(new Word(s));
