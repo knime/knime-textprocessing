b1ed8af5c40f2da9e7653618de3e6fc5dbf2c96d
David Kolb <david.kolb@knime.com>
2017-01-18 13:12:15 +0100

    Deprecate Word Vector Learner Node and create copy
    In Progress: AP-6495 (DL4J: Word2Vec Clean-up)
    https://knime-com.atlassian.net/browse/AP-6495

---
+++ org.knime.ext.textprocessing.dl4j/src/org/knime/ext/textprocessing/dl4j/nodes/embeddings/learn/WordVectorLearnerNodeDialog2.java
@@ -0,0 +1,150 @@
+/*******************************************************************************
+ * Copyright by KNIME GmbH, Konstanz, Germany
+ * Website: http://www.knime.org; Email: contact@knime.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, Version 3, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ *
+ * KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ * Hence, KNIME and ECLIPSE are both independent programs and are not
+ * derived from each other. Should, however, the interpretation of the
+ * GNU GPL Version 3 ("License") under any applicable laws result in
+ * KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ * you the additional permission to use and propagate KNIME together with
+ * ECLIPSE with only the license terms in place for ECLIPSE applying to
+ * ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ * license terms of ECLIPSE themselves allow for the respective use and
+ * propagation of ECLIPSE together with KNIME.
+ *
+ * Additional permission relating to nodes for KNIME that extend the Node
+ * Extension (and in particular that are based on subclasses of NodeModel,
+ * NodeDialog, and NodeView) and that only interoperate with KNIME through
+ * standard APIs ("Nodes"):
+ * Nodes are deemed to be separate and independent programs and to not be
+ * covered works.  Notwithstanding anything to the contrary in the
+ * License, the License does not apply to Nodes, you are not required to
+ * license Nodes under the License, and you are granted a license to
+ * prepare and propagate Nodes, in each case even if such Nodes are
+ * propagated with or for interoperation with KNIME.  The owner of a Node
+ * may freely choose the license terms applicable to such Node, including
+ * when such Node is propagated with or for interoperation with KNIME.
+ *******************************************************************************/
+package org.knime.ext.textprocessing.dl4j.nodes.embeddings.learn;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+import org.knime.core.data.NominalValue;
+import org.knime.core.data.StringValue;
+import org.knime.core.node.defaultnodesettings.DefaultNodeSettingsPane;
+import org.knime.core.node.defaultnodesettings.DialogComponentBoolean;
+import org.knime.core.node.defaultnodesettings.DialogComponentColumnNameSelection;
+import org.knime.core.node.defaultnodesettings.DialogComponentNumberEdit;
+import org.knime.core.node.defaultnodesettings.DialogComponentStringSelection;
+import org.knime.core.node.defaultnodesettings.SettingsModelBoolean;
+import org.knime.core.node.defaultnodesettings.SettingsModelDoubleBounded;
+import org.knime.core.node.defaultnodesettings.SettingsModelIntegerBounded;
+import org.knime.core.node.defaultnodesettings.SettingsModelString;
+import org.knime.ext.dl4j.base.settings.enumerate.DataParameter;
+import org.knime.ext.dl4j.base.settings.enumerate.LearnerParameter;
+import org.knime.ext.dl4j.base.settings.impl.DataParameterSettingsModels;
+import org.knime.ext.dl4j.base.settings.impl.LearnerParameterSettingsModels;
+import org.knime.ext.dl4j.base.util.EnumUtils;
+import org.knime.ext.textprocessing.data.DocumentValue;
+import org.knime.ext.textprocessing.dl4j.settings.enumerate.WordVectorLearnerParameter;
+import org.knime.ext.textprocessing.dl4j.settings.enumerate.WordVectorTrainingMode;
+import org.knime.ext.textprocessing.dl4j.settings.impl.WordVectorParameterSettingsModels;
+
+/**
+ * <code>NodeDialog</code> for the "WordVectorLearner" Node.
+ *
+ *
+ * This node dialog derives from {@link DefaultNodeSettingsPane} which allows creation of a simple dialog with standard
+ * components. If you need a more complex dialog please derive directly from {@link org.knime.core.node.NodeDialogPane}.
+ *
+ * @author David Kolb, KNIME.com GmbH
+ */
+public class WordVectorLearnerNodeDialog2 extends DefaultNodeSettingsPane {
+
+    /**
+     * New pane for configuring the WordVectorLearner node.
+     */
+    protected WordVectorLearnerNodeDialog2() {
+        final LearnerParameterSettingsModels learnerSettingsModels = new LearnerParameterSettingsModels();
+        final DataParameterSettingsModels dataSettingsModels = new DataParameterSettingsModels();
+        final WordVectorParameterSettingsModels wordVectorSettingsModels = new WordVectorParameterSettingsModels();
+
+        final SettingsModelString trainingModeSettings = (SettingsModelString)wordVectorSettingsModels
+            .createParameter(WordVectorLearnerParameter.WORD_VECTOR_TRAINING_MODE);
+
+        final SettingsModelString labelColumnSettings =
+            (SettingsModelString)dataSettingsModels.createParameter(DataParameter.LABEL_COLUMN);
+        final SettingsModelString documentColumnSettings =
+            (SettingsModelString)dataSettingsModels.createParameter(DataParameter.DOCUMENT_COLUMN);
+
+        trainingModeSettings.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(final ChangeEvent e) {
+                final WordVectorTrainingMode mode =
+                    WordVectorTrainingMode.valueOf(trainingModeSettings.getStringValue());
+                switch (mode) {
+                    case DOC2VEC:
+                        labelColumnSettings.setEnabled(true);
+                        documentColumnSettings.setEnabled(true);
+                        break;
+                    case WORD2VEC:
+                        labelColumnSettings.setEnabled(false);
+                        documentColumnSettings.setEnabled(true);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        });
+
+        addDialogComponent(new DialogComponentStringSelection(trainingModeSettings, "WordVector Training Mode",
+            EnumUtils.getStringCollectionFromToString(WordVectorTrainingMode.values())));
+        addDialogComponent(new DialogComponentBoolean(
+            (SettingsModelBoolean)wordVectorSettingsModels
+                .createParameter(WordVectorLearnerParameter.USE_BASIC_PREPROCESSING),
+            "Use Basic Token Preprocessing?"));
+        addDialogComponent(new DialogComponentNumberEdit(
+            (SettingsModelIntegerBounded)learnerSettingsModels.createParameter(LearnerParameter.SEED), "Seed", 4));
+        addDialogComponent(new DialogComponentNumberEdit(
+            (SettingsModelDoubleBounded)learnerSettingsModels.createParameter(LearnerParameter.GLOBAL_LEARNING_RATE),
+            "Learning Rate", 4));
+        addDialogComponent(new DialogComponentNumberEdit((SettingsModelDoubleBounded)wordVectorSettingsModels
+            .createParameter(WordVectorLearnerParameter.MIN_LEARNING_RATE), "Minimum Learning Rate", 4));
+        addDialogComponent(new DialogComponentNumberEdit(
+            (SettingsModelIntegerBounded)dataSettingsModels.createParameter(DataParameter.BATCH_SIZE), "Batch Size",
+            4));
+        addDialogComponent(new DialogComponentNumberEdit(
+            (SettingsModelIntegerBounded)dataSettingsModels.createParameter(DataParameter.EPOCHS), "Epochs", 4));
+        addDialogComponent(new DialogComponentNumberEdit(
+            (SettingsModelIntegerBounded)learnerSettingsModels.createParameter(LearnerParameter.TRAINING_ITERATIONS),
+            "Number of Training Iterations", 4));
+        addDialogComponent(new DialogComponentNumberEdit((SettingsModelIntegerBounded)wordVectorSettingsModels
+            .createParameter(WordVectorLearnerParameter.LAYER_SIZE), "Layer Size", 4));
+        addDialogComponent(new DialogComponentNumberEdit((SettingsModelIntegerBounded)wordVectorSettingsModels
+            .createParameter(WordVectorLearnerParameter.MIN_WORD_FREQUENCY), "Minimum Word Frequency", 4));
+        addDialogComponent(new DialogComponentNumberEdit((SettingsModelIntegerBounded)wordVectorSettingsModels
+            .createParameter(WordVectorLearnerParameter.WINDOW_SIZE), "Window Size", 4));
+
+        createNewTab("Column Selection");
+        addDialogComponent(
+            new DialogComponentColumnNameSelection(labelColumnSettings, "Label Column", 0, false, NominalValue.class));
+        addDialogComponent(new DialogComponentColumnNameSelection(documentColumnSettings, "Document Column", 0, true,
+            StringValue.class, DocumentValue.class));
+    }
+}
---
+++ org.knime.ext.textprocessing.dl4j/src/org/knime/ext/textprocessing/dl4j/nodes/embeddings/learn/WordVectorLearnerNodeModel2.java
@@ -0,0 +1,235 @@
+/*******************************************************************************
+ * Copyright by KNIME GmbH, Konstanz, Germany
+ * Website: http://www.knime.org; Email: contact@knime.org
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License, Version 3, as
+ * published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, see <http://www.gnu.org/licenses>.
+ *
+ * Additional permission under GNU GPL version 3 section 7:
+ *
+ * KNIME interoperates with ECLIPSE solely via ECLIPSE's plug-in APIs.
+ * Hence, KNIME and ECLIPSE are both independent programs and are not
+ * derived from each other. Should, however, the interpretation of the
+ * GNU GPL Version 3 ("License") under any applicable laws result in
+ * KNIME and ECLIPSE being a combined program, KNIME GMBH herewith grants
+ * you the additional permission to use and propagate KNIME together with
+ * ECLIPSE with only the license terms in place for ECLIPSE applying to
+ * ECLIPSE and the GNU GPL Version 3 applying for KNIME, provided the
+ * license terms of ECLIPSE themselves allow for the respective use and
+ * propagation of ECLIPSE together with KNIME.
+ *
+ * Additional permission relating to nodes for KNIME that extend the Node
+ * Extension (and in particular that are based on subclasses of NodeModel,
+ * NodeDialog, and NodeView) and that only interoperate with KNIME through
+ * standard APIs ("Nodes"):
+ * Nodes are deemed to be separate and independent programs and to not be
+ * covered works.  Notwithstanding anything to the contrary in the
+ * License, the License does not apply to Nodes, you are not required to
+ * license Nodes under the License, and you are granted a license to
+ * prepare and propagate Nodes, in each case even if such Nodes are
+ * propagated with or for interoperation with KNIME.  The owner of a Node
+ * may freely choose the license terms applicable to such Node, including
+ * when such Node is propagated with or for interoperation with KNIME.
+ *******************************************************************************/
+package org.knime.ext.textprocessing.dl4j.nodes.embeddings.learn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.deeplearning4j.models.embeddings.wordvectors.WordVectors;
+import org.deeplearning4j.models.paragraphvectors.ParagraphVectors;
+import org.deeplearning4j.models.word2vec.Word2Vec;
+import org.deeplearning4j.text.documentiterator.LabelAwareIterator;
+import org.deeplearning4j.text.sentenceiterator.SentenceIterator;
+import org.deeplearning4j.text.tokenization.tokenizer.preprocessor.CommonPreprocessor;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.DefaultTokenizerFactory;
+import org.deeplearning4j.text.tokenization.tokenizerfactory.TokenizerFactory;
+import org.knime.core.data.DataTableSpec;
+import org.knime.core.node.BufferedDataTable;
+import org.knime.core.node.ExecutionContext;
+import org.knime.core.node.InvalidSettingsException;
+import org.knime.core.node.defaultnodesettings.SettingsModel;
+import org.knime.core.node.port.PortObject;
+import org.knime.core.node.port.PortObjectSpec;
+import org.knime.core.node.port.PortType;
+import org.knime.ext.dl4j.base.AbstractDLNodeModel;
+import org.knime.ext.dl4j.base.settings.enumerate.DataParameter;
+import org.knime.ext.dl4j.base.settings.enumerate.LearnerParameter;
+import org.knime.ext.dl4j.base.settings.impl.DataParameterSettingsModels2;
+import org.knime.ext.dl4j.base.settings.impl.LearnerParameterSettingsModels2;
+import org.knime.ext.dl4j.base.util.ConfigurationUtils;
+import org.knime.ext.dl4j.base.util.TableUtils;
+import org.knime.ext.textprocessing.dl4j.data.BufferedDataTableLabelledDocumentIterator;
+import org.knime.ext.textprocessing.dl4j.data.BufferedDataTableSentenceIterator;
+import org.knime.ext.textprocessing.dl4j.nodes.embeddings.WordVectorPortObject;
+import org.knime.ext.textprocessing.dl4j.nodes.embeddings.WordVectorPortObjectSpec;
+import org.knime.ext.textprocessing.dl4j.settings.enumerate.WordVectorLearnerParameter;
+import org.knime.ext.textprocessing.dl4j.settings.enumerate.WordVectorTrainingMode;
+import org.knime.ext.textprocessing.dl4j.settings.impl.WordVectorParameterSettingsModels2;
+
+/**
+ * Node to learn a {@link WordVectors} model using either Wor2Vec or Doc2Vec using DL4J implementation. For details see:
+ * http://deeplearning4j.org/word2vec
+ *
+ * @author David Kolb, KNIME.com GmbH
+ */
+public class WordVectorLearnerNodeModel2 extends AbstractDLNodeModel {
+
+    /* SettingsModels */
+    private LearnerParameterSettingsModels2 m_learnerParameterSettings;
+
+    private DataParameterSettingsModels2 m_dataParameterSettings;
+
+    private WordVectorParameterSettingsModels2 m_wordVecParameterSettings;
+
+    private WordVectorPortObjectSpec m_outputSpec;
+
+    public WordVectorLearnerNodeModel2() {
+        super(new PortType[]{BufferedDataTable.TYPE}, new PortType[]{WordVectorPortObject.TYPE});
+    }
+
+    @Override
+    protected WordVectorPortObject[] execute(final PortObject[] inObjects, final ExecutionContext exec)
+        throws Exception {
+        final BufferedDataTable table = (BufferedDataTable)inObjects[0];
+
+        TableUtils.checkForEmptyTable(table);
+
+        final WordVectorTrainingMode mode =
+            WordVectorTrainingMode.valueOf(m_wordVecParameterSettings.getString(WordVectorLearnerParameter.WORD_VECTOR_TRAINING_MODE));
+        final String labelColumnName = m_dataParameterSettings.getString(DataParameter.LABEL_COLUMN);
+        final String documentColumnName = m_dataParameterSettings.getString(DataParameter.DOCUMENT_COLUMN);
+        WordVectors wordVectors = null;
+
+        // training parameters
+        final int trainingIterations = m_learnerParameterSettings.getInteger(LearnerParameter.TRAINING_ITERATIONS);
+        final int minWordFrequency = m_wordVecParameterSettings.getInteger(WordVectorLearnerParameter.MIN_WORD_FREQUENCY);
+        final int layerSize = m_wordVecParameterSettings.getInteger(WordVectorLearnerParameter.LAYER_SIZE);
+        final int seed = m_learnerParameterSettings.getInteger(LearnerParameter.SEED);
+        final double learningRate = m_learnerParameterSettings.getDouble(LearnerParameter.GLOBAL_LEARNING_RATE);
+        final double minLearningRate = m_wordVecParameterSettings.getDouble(WordVectorLearnerParameter.MIN_LEARNING_RATE);
+        final int windowSize = m_wordVecParameterSettings.getInteger(WordVectorLearnerParameter.WINDOW_SIZE);
+        final int epochs = m_dataParameterSettings.getInteger(DataParameter.EPOCHS);
+        final int batchSize = m_dataParameterSettings.getInteger(DataParameter.BATCH_SIZE);
+
+        // sentence tokenizer and preprocessing
+        final boolean usePreproc = m_wordVecParameterSettings.getBoolean(WordVectorLearnerParameter.USE_BASIC_PREPROCESSING);
+        final TokenizerFactory t = new DefaultTokenizerFactory();
+        if (usePreproc) {
+            t.setTokenPreProcessor(new CommonPreprocessor());
+        }
+
+        switch (mode) {
+            case DOC2VEC:
+                final LabelAwareIterator docIter =
+                    new BufferedDataTableLabelledDocumentIterator(table, documentColumnName, labelColumnName);
+
+                // build doc2vec model
+                final ParagraphVectors d2v = new ParagraphVectors.Builder().learningRate(learningRate)
+                    .minLearningRate(minLearningRate).seed(seed).layerSize(layerSize).batchSize(batchSize)
+                    .windowSize(windowSize).minWordFrequency(minWordFrequency).iterations(trainingIterations)
+                    .epochs(epochs).iterate(docIter).trainWordVectors(true).tokenizerFactory(t).build();
+
+                d2v.fit();
+                wordVectors = d2v;
+
+                break;
+
+            case WORD2VEC:
+                final SentenceIterator sentenceIter = new BufferedDataTableSentenceIterator(table, documentColumnName);
+
+                // build word2vec model
+                final Word2Vec w2v = new Word2Vec.Builder().learningRate(learningRate).minLearningRate(minLearningRate)
+                    .seed(seed).layerSize(layerSize).batchSize(batchSize).windowSize(windowSize)
+                    .minWordFrequency(minWordFrequency).iterations(trainingIterations).epochs(epochs)
+                    .iterate(sentenceIter).tokenizerFactory(t).build();
+
+                w2v.fit();
+                wordVectors = w2v;
+
+                break;
+
+            default:
+                throw new InvalidSettingsException("No case defined for WordVectorTrainingMode: " + mode);
+        }
+
+        final WordVectorPortObject outPortObject = new WordVectorPortObject(wordVectors, m_outputSpec);
+        return new WordVectorPortObject[]{outPortObject};
+    }
+
+    @Override
+    protected WordVectorPortObjectSpec[] configure(final PortObjectSpec[] inSpecs) throws InvalidSettingsException {
+        final DataTableSpec tableSpec = (DataTableSpec)inSpecs[0];
+
+        final WordVectorTrainingMode mode =
+                WordVectorTrainingMode.valueOf(m_wordVecParameterSettings.getString(WordVectorLearnerParameter.WORD_VECTOR_TRAINING_MODE));
+        final String labelColumnName = m_dataParameterSettings.getString(DataParameter.LABEL_COLUMN);
+        final String documentColumnName = m_dataParameterSettings.getString(DataParameter.DOCUMENT_COLUMN);
+
+        switch (mode) {
+            case DOC2VEC:
+                try {
+                    ConfigurationUtils.validateColumnSelection(tableSpec,
+                        new String[]{labelColumnName, documentColumnName});
+                } catch (final InvalidSettingsException e) {
+                    throw new InvalidSettingsException("Need to specify document and label column for " + mode);
+                }
+                break;
+            case WORD2VEC:
+                try {
+                    ConfigurationUtils.validateColumnSelection(tableSpec, documentColumnName);
+                } catch (final InvalidSettingsException e) {
+                    throw new InvalidSettingsException("Need to specify document column for " + mode);
+                }
+                break;
+            default:
+                throw new InvalidSettingsException("No case defined for WordVectorTrainingsMode: " + mode);
+        }
+
+        m_outputSpec = new WordVectorPortObjectSpec(mode);
+        return new WordVectorPortObjectSpec[]{m_outputSpec};
+    }
+
+    @Override
+    protected List<SettingsModel> initSettingsModels() {
+        m_learnerParameterSettings = new LearnerParameterSettingsModels2();
+        m_learnerParameterSettings.setParameter(LearnerParameter.GLOBAL_LEARNING_RATE);
+        m_learnerParameterSettings.setParameter(LearnerParameter.TRAINING_ITERATIONS);
+        m_learnerParameterSettings.setParameter(LearnerParameter.SEED);
+
+        m_dataParameterSettings = new DataParameterSettingsModels2();
+        m_dataParameterSettings.setParameter(DataParameter.BATCH_SIZE);
+        m_dataParameterSettings.setParameter(DataParameter.EPOCHS);
+        m_dataParameterSettings.setParameter(DataParameter.LABEL_COLUMN);
+        // default training mode is Word2Vec so labels are not required by
+        // default
+        m_dataParameterSettings.getParameter(DataParameter.LABEL_COLUMN).setEnabled(false);
+
+        m_dataParameterSettings.setParameter(DataParameter.DOCUMENT_COLUMN);
+
+        m_wordVecParameterSettings = new WordVectorParameterSettingsModels2();
+        m_wordVecParameterSettings.setParameter(WordVectorLearnerParameter.LAYER_SIZE);
+        m_wordVecParameterSettings.setParameter(WordVectorLearnerParameter.MIN_WORD_FREQUENCY);
+        m_wordVecParameterSettings.setParameter(WordVectorLearnerParameter.WINDOW_SIZE);
+        m_wordVecParameterSettings.setParameter(WordVectorLearnerParameter.WORD_VECTOR_TRAINING_MODE);
+        m_wordVecParameterSettings.setParameter(WordVectorLearnerParameter.MIN_LEARNING_RATE);
+        m_wordVecParameterSettings.setParameter(WordVectorLearnerParameter.USE_BASIC_PREPROCESSING);
+
+        final List<SettingsModel> settings = new ArrayList<>();
+        settings.addAll(m_learnerParameterSettings.getAllInitializedSettings());
+        settings.addAll(m_dataParameterSettings.getAllInitializedSettings());
+        settings.addAll(m_wordVecParameterSettings.getAllInitializedSettings());
+
+        return settings;
+    }
+
+}
