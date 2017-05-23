a36ae586aa030ac77278cf0bba2672ffccf9a4ec
Julian Bunzel <jbunzel@zedat.fu-berlin.de>
2017-05-23 12:00:42 +0200

    AP-7254: Added French Language Feature and Plugin
    IN BACKLOG: AP-7254 (Text Mining: German Language Feature)
    https://knime-com.atlassian.net/browse/AP-7254

---
+++ org.knime.ext.textprocessing.language.french/src/org/knime/ext/textprocessing/language/french/TextprocessingFrenchLanguagePack.java
@@ -0,0 +1,82 @@
+package org.knime.ext.textprocessing.language.french;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+import org.eclipse.core.runtime.FileLocator;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.resource.ImageDescriptor;
+import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.knime.core.node.NodeLogger;
+import org.osgi.framework.Bundle;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
+
+/**
+ * The activator class controls the plug-in life cycle
+ */
+public class TextprocessingFrenchLanguagePack extends AbstractUIPlugin {
+
+	// The plug-in ID
+	public static final String PLUGIN_ID = "org.knime.ext.textprocessing.language.french";
+
+	// The shared instance
+	private static TextprocessingFrenchLanguagePack plugin;
+	
+	/**
+	 * The constructor
+	 */
+	public TextprocessingFrenchLanguagePack() {
+		plugin = this;
+	}
+
+    /**
+     * This method is called when the plug-in is stopped.
+     * @param context The bundle context.
+     * @throws Exception If cause by super class.
+     */
+    @Override
+	public void stop(BundleContext context) throws Exception {
+		plugin = null;
+		super.stop(context);
+	}
+
+	/**
+	 * Returns the shared instance
+	 *
+	 * @return the shared instance
+	 */
+	public static TextprocessingFrenchLanguagePack getDefault() {
+		return plugin;
+	}
+
+	/**
+	 * Returns an image descriptor for the image file at the given
+	 * plug-in relative path
+	 *
+	 * @param path the path
+	 * @return the image descriptor
+	 */
+	public static ImageDescriptor getImageDescriptor(String path) {
+		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+	
+    /**
+     * Resolves a path relative to the plug-in or any fragment's root into an absolute path.
+     *
+     * @param relativePath a relative path
+     * @return the resolved absolute path
+     */
+    public static File resolvePath(final String relativePath) {
+        Bundle myself = FrameworkUtil.getBundle(TextprocessingFrenchLanguagePack.class);
+        try {
+            URL fileUrl = FileLocator.toFileURL(FileLocator.find(myself, new Path(relativePath), null));
+            return new File(fileUrl.getPath());
+        } catch (IOException ex) {
+            NodeLogger.getLogger(TextprocessingFrenchLanguagePack.class)
+                .error("Could not resolve relativ path '" + relativePath + "': " + ex.getMessage(), ex);
+            return new File("");
+        }
+    }
+}
