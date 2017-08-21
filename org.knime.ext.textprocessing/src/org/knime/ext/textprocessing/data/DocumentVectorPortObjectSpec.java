4d309573bb53e9c69acf9d4673a9ac9db2c5cdb0
Julian Bunzel <jbunzel@zedat.fu-berlin.de>
2017-08-21 14:26:05 +0200

    AP-6753: Adjusted view
    Task-Url: https://knime-com.atlassian.net/browse/AP-6753

--- org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/data/DocumentVectorPortObjectSpec.java
+++ org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/data/DocumentVectorPortObjectSpec.java
@@ -173,7 +173,7 @@ public class DocumentVectorPortObjectSpec extends AbstractSimplePortObjectSpec {
         htmlText.append("<tr class=\"odd\"><td>Vector Value</td><td>").append(m_vectorValue).append("</td></tr>\n");
         htmlText.append("<tr class=\"even\"><td>As Collection Cell</td><td>").append(m_asCollectionCell)
             .append("</td></tr>\n");
-        htmlText.append("<tr class=\"even\"><td>Feature Space Columns</td><td>")
+        htmlText.append("<tr class=\"odd\"><td>Feature Space Columns</td><td>")
             .append(String.join(", ", m_featureSpaceColumns)).append("</td></tr>\n");
         htmlText.append("</table></body></html>");
         JEditorPane tablePane = new JEditorPane("text/html", "");
