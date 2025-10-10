7454fb74eafcfce5e2e572cc0af3f449aa450fdd
Robin Gerling <robin.gerling@knime.com>
2025-10-10 15:32:26 +0200

    UIEXT-2987: WebUI Migration of Bag Of Words Creator
    UIEXT-2987 (Web UI Bag of words creator)

--- org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/nodes/transformation/bow/BagOfWordsNodeFactory2.java
+++ org.knime.ext.textprocessing/src/org/knime/ext/textprocessing/nodes/transformation/bow/BagOfWordsNodeFactory2.java
@@ -50,23 +50,37 @@ package org.knime.ext.textprocessing.nodes.transformation.bow;
 import org.knime.core.node.NodeDialogPane;
 import org.knime.core.node.NodeFactory;
 import org.knime.core.node.NodeView;
+import org.knime.core.webui.node.dialog.NodeDialog;
+import org.knime.core.webui.node.dialog.NodeDialogFactory;
+import org.knime.core.webui.node.dialog.NodeDialogManager;
+import org.knime.core.webui.node.dialog.SettingsType;
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultNodeDialog;
+import org.knime.core.webui.node.dialog.defaultdialog.DefaultKaiNodeInterface;
+import org.knime.core.node.NodeFactory.NodeType;
+import org.knime.core.webui.node.dialog.kai.KaiNodeInterface;
+import org.knime.core.webui.node.dialog.kai.KaiNodeInterfaceFactory;
+import org.knime.core.node.NodeDescription;
+import org.knime.node.impl.description.DefaultNodeDescriptionUtil;
+import java.util.Map;
+import org.knime.node.impl.description.PortDescription;
+import java.util.List;
+import static org.knime.node.impl.description.PortDescription.fixedPort;
 
 /**
  * The {@link org.knime.core.node.NodeFactory} of the bag of words creator, provides methods to create the model and the
  * dialog instance.
  *
  * @author Kilian Thiel, University of Konstanz
+ * @author Robin Gerling, KNIME GmbH, Konstanz, Germany
+ * @author AI Migration Pipeline v1.2
  * @since 3.5
  */
-public class BagOfWordsNodeFactory2 extends NodeFactory<BagOfWordsNodeModel2> {
+@SuppressWarnings("restriction")
+public class BagOfWordsNodeFactory2 extends NodeFactory<BagOfWordsNodeModel2> implements NodeDialogFactory, KaiNodeInterfaceFactory {
 
     /**
      * {@inheritDoc}
      */
-    @Override
-    protected NodeDialogPane createNodeDialogPane() {
-        return new BagOfWordsNodeDialog2();
-    }
 
     /**
      * {@inheritDoc}
@@ -98,5 +112,58 @@ public class BagOfWordsNodeFactory2 extends NodeFactory<BagOfWordsNodeModel2> {
     @Override
     protected boolean hasDialog() {
         return true;
+    }
+    private static final String NODE_NAME = "Bag Of Words Creator";
+    private static final String NODE_ICON = "../../../icons/TextProcDefault.png";
+    private static final String SHORT_DESCRIPTION = """
+            Bag of words creator.
+            """;
+    private static final String FULL_DESCRIPTION = """
+            This node creates a bag of words (BoW) of a set of documents. A BoW consists of at least one column
+                containing the terms occurring in the corresponding document. All term related columns like the document
+                column can be selected in the node dialog and will be copied to the output table.
+            """;
+    private static final List<PortDescription> INPUT_PORTS = List.of(
+            fixedPort("Documents input table", """
+                The input table containing the documents.
+                """)
+    );
+    private static final List<PortDescription> OUTPUT_PORTS = List.of(
+            fixedPort("Documents output table", """
+                An output table containing the bag of words.
+                """)
+    );
+
+    @Override
+    public NodeDialogPane createNodeDialogPane() {
+        return NodeDialogManager.createLegacyFlowVariableNodeDialog(createNodeDialog());
+    }
+
+    @Override
+    public NodeDialog createNodeDialog() {
+        return new DefaultNodeDialog(SettingsType.MODEL, BagOfWordsNodeFactory2Parameters.class);
+    }
+
+    @Override
+    public NodeDescription createNodeDescription() {
+        return DefaultNodeDescriptionUtil.createNodeDescription(
+            NODE_NAME,
+            NODE_ICON,
+            INPUT_PORTS,
+            OUTPUT_PORTS,
+            SHORT_DESCRIPTION,
+            FULL_DESCRIPTION,
+            List.of(),
+            BagOfWordsNodeFactory2Parameters.class,
+            null,
+            NodeType.Manipulator,
+            List.of(),
+            null
+        );
+    }
+
+    @Override
+    public KaiNodeInterface createKaiNodeInterface() {
+        return new DefaultKaiNodeInterface(Map.of(SettingsType.MODEL, BagOfWordsNodeFactory2Parameters.class));
     }
 }
